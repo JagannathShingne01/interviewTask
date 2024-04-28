3634641d6b736b9ac85dfbab4ee6b3688acfd721
Jagannath Shingne <133676629+JagannathShingne01@users.noreply.github.com>
2024-04-28 17:53:53 +0530

    Update DbConnection.js

--- server/database/DbConnection.js
+++ server/database/DbConnection.js
@@ -4,7 +4,7 @@ import mongoose  from "mongoose";
 const connectionToDb = async() =>{
     try{
     const { connection } = await mongoose.connect(
-        "mongodb+srv://adminpw:admin123@lmspw.rjvxc1n.mongodb.net/s?retryWrites=true&w=majority"
+        ""
     );
     if(connection){
         console.log(`Connected to Db:${connection.host}`);
